27633a95742c66624d7516dbdaf464866e441241
dnyana <dnyaneshgawade903@gmail.com>
2021-08-28 09:22:25 +0530

    [Dnyanesh] Add : Perform Aggregate Function On Normalize Table

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -153,3 +153,137 @@ CREATE TABLE Employee_Payroll (
 
   Update Employee_Payroll 
   set Basic_Pay=50000 ,Deductions=10000, Taxable_Pay=7000,Income_Tax=4000,Net_Pay=40000 where Id=8
+
+
+
+  
+create table EmpDetails
+  (
+		EmpId int identity(1,1) primary key,
+		Name varchar(20) not null,
+		Phone_Number float not null,
+		Address varchar(20) not null,
+		Joining_date date not null,
+		Dept_id int not null
+  )
+  alter table EmpDetails add GenderId int not null 
+
+  create table SalaryDetails
+  (
+		Sal_Id int identity(1,1) primary key,
+		Emp_Id int ,
+		Basic_pay float not null,
+		Deduction float not null,
+		Taxable_Pay float not null,
+		Income_Tax float not null,
+		Net_Pay float not null,
+  )
+  
+
+  create table DeptDetails
+  (
+		DeptId int identity(1,1) primary key,
+		Department_name varchar(20) not null,
+  )
+
+  create table GenderInfo
+  (
+		GenderId int identity(1,1) primary Key,
+		Gender varchar(10) not null
+  )
+
+  ALTER TABLE EmpDetails
+  ADD FOREIGN KEY (Dept_Id) 
+  REFERENCES DeptDetails(DeptId);
+
+  ALTER TABLE EmpDetails
+  ADD FOREIGN KEY (GenderId) 
+  REFERENCES GenderInfo(GenderId)
+
+  INSERT INTO EmpDetails(Name,Phone_Number,Address,Joining_Date,Dept_Id,GenderId)  
+  VALUES 
+  ('Dnyanesh',7878787878,'Pune','2018-01-03',1,'1'),
+  ('Mark',9898989898,'Goa','2019-11-13',2,'1'),
+  ('Charlie',9999999999,'Mumbai','2020-05-21',3,'1'),
+  ('Bobby',8888888888,'Chennai','2017-01-03',4,'1'),
+  ('Yash',7777777777,'Delhi','2016-10-15',1,'1'),
+  ('Saumya',8888880000,'Kolkata','2017-01-03',4,'2'),
+  ('Rutuja',7777770000,'Panjab','2016-10-15',1,'2'),
+  ('Terissa',8989890000,'Keral','2017-01-03',2,'2'),
+  ('Terissa',7878780000,'Bangal','2016-10-15',3,'2')
+
+
+  INSERT INTO DeptDetails (Department_name)
+  VALUES
+  ('Developer'),
+  ('Salse'),
+  ('Marketing'),
+  ('Hr')
+
+  INSERT INTO SalaryDetails (Emp_Id,Basic_pay,Deduction,Taxable_Pay,Income_Tax,Net_Pay)
+  VALUES
+  (10,50000,12000,6000,4000,38000)
+
+  (1,30000,5000,2000,1000,25000),
+  (2,35000,7000,3000,2000,27000),
+  (3,28000,4000,2000,1000,24000),
+  (4,40000,8000,4000,3000,32000),
+  (5,44000,10000,5000,4000,34000),
+  (6,33000,5000,2000,1500,28000),
+  (7,37000,5000,3000,1000,32000),
+  (8,45000,10000,5000,3000,35000),
+  (9,50000,12000,6000,4000,38000),
+  (10,50000,12000,6000,4000,38000)
+
+
+  INSERT INTO GenderInfo (Gender)
+  VALUES
+  ('Male'),
+  ('Female')
+
+
+  ALTER TABLE SalaryDetails
+  ADD FOREIGN KEY (Emp_Id) 
+  REFERENCES EmpDetails(EmpId)
+
+  select * From EmpDetails
+  select * From SalaryDetails
+
+  truncate table SalaryDetails
+
+
+
+--UC11
+
+
+SELECT SUM(SalaryDetails.Basic_pay)
+FROM SalaryDetails
+JOIN EmpDetails
+ON EmpDetails.EmpId = SalaryDetails.Emp_Id
+GROUP BY EmpDetails.GenderId
+
+SELECT MIN(SalaryDetails.Basic_pay)
+FROM SalaryDetails
+JOIN EmpDetails
+ON EmpDetails.EmpId = SalaryDetails.Emp_Id
+GROUP BY EmpDetails.GenderId
+
+SELECT MAX(SalaryDetails.Basic_pay)
+FROM SalaryDetails
+JOIN EmpDetails
+ON EmpDetails.EmpId = SalaryDetails.Emp_Id
+GROUP BY EmpDetails.GenderId
+
+SELECT AVG(SalaryDetails.Basic_pay)
+FROM SalaryDetails
+JOIN EmpDetails
+ON EmpDetails.EmpId = SalaryDetails.Emp_Id
+GROUP BY EmpDetails.GenderId
+
+SELECT COUNT(SalaryDetails.Basic_pay)
+FROM SalaryDetails
+JOIN EmpDetails
+ON EmpDetails.EmpId = SalaryDetails.Emp_Id
+GROUP BY EmpDetails.GenderId
+
+
